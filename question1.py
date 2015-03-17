b891251e14e8b551d4cba0a5a67a0276814e94d2
zhileiyue <zhilei.yue@gmail.com>
2015-03-16 22:29:17 -0400

    improve question1.py

--- question1.py
+++ question1.py
@@ -1,11 +1,14 @@
 import sys
+import io
 def parsefile(test):
     try:
-        f = open(test,'r')
+        f = io.open(test, 'r', encoding='utf-8')
         lines = f.readlines()
     except IOError as e:
         print e
+        return
     else:
+        print 'File opened successfully!'
         cnt=0
         sum=0.0
         for line in lines:           
@@ -16,5 +19,11 @@ def parsefile(test):
                 cnt=cnt+1
         print 'The total count is:'+str(cnt)
         print 'The total value is:'+str(sum)
+        f.close()
+        if f.closed==True:
+            print 'The file has been closed'
+        else:
+            print 'The file is being processed.'
+        return
 if __name__ == '__main__':
-    parsefile('/Users/zhileiyue/Desktop/1.txt')
+    parsefile(sys.argv[1])
